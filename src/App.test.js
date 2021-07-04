ee547bd0e2ec829095458e9863a5c63af4ca251b
Muhammad Abdullah <kb337137@gmail.com>
2021-07-04 15:00:34 -0700

    react components testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello world', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hello/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('find world in the document', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/world/i);
   expect(linkElement).toBeInTheDocument();
 });
